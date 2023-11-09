15bd7618823f87464a4b7356112bb9018ca39c83
YahirR182 <130204482+YahirR182@users.noreply.github.com>
2023-11-09 00:13:05 -0600

    T5
    Subí carpeta T5 DOM

---
+++ 02_WebDevelopment/T5-DOM/DataTypes/DataTypes.Web/Program.cs
@@ -0,0 +1,29 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Agregar servicios a la colección de servicios.
+builder.Services.AddControllersWithViews(); 
+
+var app = builder.Build();
+
+// Configure el pipeline de solicitudes HTTP.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+});
+
+
+
+app.Run();
