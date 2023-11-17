e85a8a126cf1b5ea8e8125fadd530b575c4413c0
YahirR182 <130204482+YahirR182@users.noreply.github.com>
2023-11-16 21:57:40 -0600

    E4
    Se agregó la carpeta E4

---
+++ 02_WebDevelopment/E4/GymManager.Web/GymManager.Web/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace GymManager.Web.Models
+{
+	public class Member
+	{
+
+		public string Name { get; set; }
+		public string LastName { get; set; }
+		public DateTime BirthDay { get; set; }
+		public int CityId { get; set; }
+		public string Email { get; set; }
+		public bool AllowNewsLetter { get; set; }
+
+    }
+}
+
---
+++ 02_WebDevelopment/E4/GymManager.Web/GymManager.Web/Program.cs
@@ -0,0 +1,36 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseEndpoints(endpoints => {
+
+    endpoints.MapControllerRoute("memberIn", "Attendance/MemberIn", new { controller = "Attendance", action = "MemberIn" });
+    endpoints.MapControllerRoute("memberOut", "Attendance/MemberOut", new { controller = "Attendance", action = "MemberOut" });
+
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+   
+    endpoints.MapRazorPages();
+});
+
+app.MapRazorPages();
+
+app.Run();
+
