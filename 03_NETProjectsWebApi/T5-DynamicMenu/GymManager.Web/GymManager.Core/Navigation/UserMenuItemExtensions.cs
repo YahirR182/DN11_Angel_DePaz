6290693bc7b56f867a3a42606398221c2d145b4d
YahirR182 <130204482+YahirR182@users.noreply.github.com>
2023-11-22 18:26:44 -0600

    T5

---
+++ 03_NETProjectsWebApi/T5-DynamicMenu/GymManager.Web/GymManager.Core/Navigation/UserMenuItemExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace GymManager.Core.Navigation
+{
+    public static class UserMenuItemExtensions
+    {
+        public static bool IsMenuActive(this UserMenuItem menuItem, string currentPageName)
+        {
+            if (menuItem.Name == currentPageName)
+            {
+                return true;
+
+            }
+            if (menuItem.Items != null)
+            {
+                foreach (var subMenuItem in menuItem.Items)
+                {
+                    if (subMenuItem.IsMenuActive(currentPageName))
+                    {
+                        return true;
+
+
+
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
+
---
+++ 03_NETProjectsWebApi/T5-DynamicMenu/GymManager.Web/GymManager.Web/Pages/Shared/Components/AppMenu/AppMenuViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using GymManager.ApplicationServices.Navigation;
+using GymManager.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymManager.Web.Pages.Shared.Components.AppMenu
+{
+	public class AppMenuViewComponent: ViewComponent
+	{
+
+		private readonly IMenuAppService _menuAppService;
+
+		public AppMenuViewComponent(IMenuAppService menuAppService)
+		{
+			_menuAppService = menuAppService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(string currentPageName=null)
+		{
+			
+			var model = new MenuViewModel
+			{
+				CurrentPageName = currentPageName,
+				Menu=_menuAppService.GetMenu()
+
+			};
+
+
+            return View("Default",model);
+		}
+	}
+	
+}
+
